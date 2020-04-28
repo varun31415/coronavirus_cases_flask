dd348a3f9fb3a5517980c6b1bf350f0b46f54ee6
Varun Rajkumar <pi.fermet@gmail.com>
2020-04-28 15:16:48 -0700

    commit

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import threading
 from flask_socketio import SocketIO, emit
+import COVID19Py
+
+global coronavirus
+coronavirus = COVID19Py.COVID19()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "2e8fe3d23ehi23e7y"
@@ -13,12 +17,35 @@ socket = SocketIO(app)
 
 global info
 
+
+def cases_update_countries():
+    threading.Timer(10.0,cases_update_countries)
+    locations = coronavirus.getLocations(rank_by="confirmed")
+    country_names = []
+    for location in locations:
+        if location["country"] not in country_names:
+            country_names.append(location["country"])
+    countries = []
+    for name in country_names:
+        deaths = 0
+        confirmed = 0
+        for location in locations:
+            if location["country"] == name:
+                deaths = deaths + location["latest"]["deaths"]
+                confirmed = confirmed + location["latest"]["confirmed"]
+        countries.append({"country":name,"deaths":deaths,"confirmed":confirmed})
+    countries = sorted(countries, key=lambda country: country["confirmed"],reverse=True) 
+    code1 = ""
+    for country in countries:
+        code1 = code1 + f"""<tr id="{country["country"]}"><td>{country['country']}</td><td>{ country["confirmed"] }</td><td>{country["deaths"]}</td></tr>"""
+    code = code1
+    return code1
+
 def render_page(template, *args, **kwargs):
     return render_template("base.html", template=template, *args, **kwargs)
 
 def coronavirus_cases():
-    global socket
-    threading.Timer(4.0,coronavirus_cases)
+    threading.Timer(10.0,coronavirus_cases)
     res = requests.get('https://www.worldometers.info/coronavirus/')
     html_page = res.content
     soup = BeautifulSoup(html_page, 'html.parser')
@@ -70,10 +97,11 @@ def coronavirus_cases():
 
 
 info = coronavirus_cases()
+code = cases_update_countries()
 
-@socket.on("update request")
+@socket.on("update request",namespace="/")
 def update(info_updated):
-    socket.emit("update",info)
+    socket.emit("update",info,namespace='/')
 
 @app.route("/")
 def index():
@@ -82,6 +110,11 @@ def index():
 @app.route("/donate")
 def donater():
     return render_page("donate.html")
+
+@app.route('/countries')
+def show_countries():
+    global code
+    return render_page("countries.html",code=code)
 
 if __name__ == "__main__":
     socket.run(app,port=80,host="0.0.0.0")
